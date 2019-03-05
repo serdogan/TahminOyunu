a224201fed5e2c65894e81cb95b783a26a49545d
Sefa Erdogan <serdogan132@gmail.com>
2019-03-05 14:56:28 +0300

    Tahmin Oyunu
    C# ile yapılmış tahmin oyunu

---
+++ cstech/Models/Tahminler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cstech.Models
+{
+    /// <summary>
+    /// DataGridViewRow'a kullanıcının tahminlerini yazmak için kullanılıyor
+    /// </summary>
+    public class Tahminler
+    {
+        [Description("Tahmin Sırası")]
+        public int TahminSirasi { get; set; }
+
+        [Description("Değer")]
+        public int Deger { get; set; }
+    }
+}
